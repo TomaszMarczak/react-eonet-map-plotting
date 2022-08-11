97e043110d020db31426c0e449a370cb8c2e68ab
Tomasz Marczak <tommy.marczak@gmail.com>
2022-08-11 13:26:33 +0200

    Plotting  wildfires, volcanoes and icebergs

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,7 +1,74 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import { volcanoIcon } from "../resources/Markers";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { icebergIcon, volcanoIcon, wildfireIcon } from "../resources/Markers";
 
 export default function MapComponent({ eventData }) {
+  const volcanoes = eventData.map((event) => {
+    if (event.categories[0].id === "volcanoes") {
+      return (
+        <Marker
+          position={[
+            event.geometry[0].coordinates[1],
+            event.geometry[0].coordinates[0],
+          ]}
+          icon={volcanoIcon}
+        >
+          <Popup>
+            <ul>
+              <li>{event.title}</li>
+              <li>
+                More info: <a href={event.sources[0].url}>Link</a>
+              </li>
+            </ul>
+          </Popup>
+        </Marker>
+      );
+    }
+    return null;
+  });
+
+  const icebergs = eventData.map((event) => {
+    if (event.categories[0].id === "wildfires") {
+      return (
+        <Marker
+          position={[
+            event.geometry[0].coordinates[1],
+            event.geometry[0].coordinates[0],
+          ]}
+          icon={wildfireIcon}
+        >
+          <Popup>
+            <ul>
+              <li>{event.title}</li>
+              <li>
+                More info: <a href={event.sources[0].url}>Link</a>
+              </li>
+            </ul>
+          </Popup>
+        </Marker>
+      );
+    }
+    return null;
+  });
+  const wildfires = eventData.map((event) => {
+    if (event.categories[0].id === "seaLakeIce") {
+      return (
+        <Marker
+          position={[
+            event.geometry[0].coordinates[1],
+            event.geometry[0].coordinates[0],
+          ]}
+          icon={icebergIcon}
+        >
+          <Popup>
+            <ul>
+              <li>{event.title}</li>
+            </ul>
+          </Popup>
+        </Marker>
+      );
+    }
+    return null;
+  });
   return (
     <MapContainer
       center={[41.948081, -7.7007248]}
@@ -12,7 +79,9 @@ export default function MapComponent({ eventData }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[41.948081, -7.7007248]} icon={volcanoIcon} />
+      {volcanoes}
+      {wildfires}
+      {icebergs}
     </MapContainer>
   );
 }
--- src/resources/Markers.js
+++ src/resources/Markers.js
@@ -2,10 +2,29 @@ import L from "leaflet";
 import { renderToStaticMarkup } from "react-dom/server";
 
 // Markers conversion
-const volcano = renderToStaticMarkup(<i className="fa-solid fa-volcano"></i>);
+const volcano = renderToStaticMarkup(
+  <div className="redPin">
+    <i className="fa-solid fa-volcano"></i>
+  </div>
+);
+const wildfire = renderToStaticMarkup(
+  <div className="orangePin">
+    <i className="fa-solid fa-fire"></i>
+  </div>
+);
+const iceberg = renderToStaticMarkup(
+  <div className="bluePin">
+    <i class="fa-solid fa-snowflake"></i>
+  </div>
+);
 
 // Markers list
 export const volcanoIcon = new L.divIcon({
   html: volcano,
-  className: "volcanoIcon",
+});
+export const wildfireIcon = new L.divIcon({
+  html: wildfire,
+});
+export const icebergIcon = new L.divIcon({
+  html: iceberg,
 });
