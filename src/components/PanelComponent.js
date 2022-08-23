3ffee6b92847633c8fb57fc5fcb5e6d9e0e4e642
Tomasz Marczak <tommy.marczak@gmail.com>
2022-08-23 08:35:59 +0200

    Minor changes to UI

--- src/components/PanelComponent.js
+++ src/components/PanelComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card, Stack, ToggleButton } from "react-bootstrap";
+import { Card, Stack, Button, ButtonGroup } from "react-bootstrap";
 import { useApp } from "../contexts/AppContext";
+import { useEffect } from "react";
 
 export default function PanelComponent() {
   const {
@@ -15,45 +16,41 @@ export default function PanelComponent() {
     icebergs,
   } = useApp();
 
+  useEffect(() => {
+    console.log("----------------------------");
+    console.log(`Volcanoes: ${checkedVolcanoes}`);
+    console.log(`Wildfires: ${checkedWildfires}`);
+    console.log(`Icebergs: ${checkedIcebergs}`);
+  }, [checkedIcebergs, checkedVolcanoes, checkedWildfires]);
+
   return (
     <div style={{ zIndex: "1000", position: "absolute", right: 0 }}>
       <Card className="m-4 p-2">
         <Card.Body>
           <Card.Title className="py-1">Panel</Card.Title>
           <Stack>
-            <ToggleButton
-              className="mb-2"
-              id="toggle-check-volcanoes"
-              type="checkbox"
-              variant="outline-primary"
-              checked={checkedVolcanoes}
-              value="volcanoes"
-              onChange={() => setCheckedVolcanoes(!checkedVolcanoes)}
-            >
-              Volcanoes
-            </ToggleButton>
-            <ToggleButton
-              className="mb-2"
-              id="toggle-check-wildfires"
-              type="checkbox"
-              variant="outline-primary"
-              checked={checkedWildfires}
-              value="wildfires"
-              onChange={() => setCheckedWildfires(!checkedWildfires)}
-            >
-              Wildfires
-            </ToggleButton>
-            <ToggleButton
-              className="mb-2"
-              id="toggle-check-icebergs"
-              type="checkbox"
-              variant="outline-primary"
-              checked={checkedIcebergs}
-              value="icebergs"
-              onChange={() => setCheckedIcebergs(!checkedIcebergs)}
-            >
-              Icebergs
-            </ToggleButton>
+            <ButtonGroup vertical>
+              <Button
+                variant={checkedVolcanoes ? "primary" : "secondary"}
+                onClick={() => setCheckedVolcanoes(!checkedVolcanoes)}
+              >
+                Volcanoes
+              </Button>
+              <Button
+                variant={checkedWildfires ? "primary" : "secondary"}
+                onClick={() => setCheckedWildfires(!checkedWildfires)}
+              >
+                Wildfires
+              </Button>
+              <Button
+                className="mb-2"
+                variant={checkedIcebergs ? "primary" : "secondary"}
+                onClick={() => setCheckedIcebergs(!checkedIcebergs)}
+              >
+                Icebergs
+              </Button>
+            </ButtonGroup>
+
             <Card.Text>
               {volcanoes.length} Volcanoes <br />
               {wildfires.length} Wildfires <br />
